4bb4bd5a49c6de42169a5c5e9c8762626a416c6e
Ryan Baker <rbaker87@gmail.com>
2026-01-28 17:26:08 -0500

    Fixed incorrect export version.

--- Autoloads.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-
-var clue_count: int = 1
---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'escape-game-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './escape-game.html',
+  './escape-game.js',
+  './escape-game.wasm',
+  './escape-game.pck',
+  './escape-game.png',
+  './escape-game.icon.png',
+  './escape-game.apple-touch-icon.png',
+  './escape-game.audio.worklet.js',
+  './escape-game.audio.position.worklet.js',
+  './manifest.json'
+];
+
+// Install event - cache all assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching game assets...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => {
+      return self.skipWaiting();
+    })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((response) => {
+        // Don't cache non-successful responses or non-GET requests
+        if (!response || response.status !== 200 || event.request.method !== 'GET') {
+          return response;
+        }
+        // Clone and cache the response
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
